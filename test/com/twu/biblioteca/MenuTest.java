4379ba19e66f1fddec28deda8ed99c136152f254
Zhu Mei <meizhu812@outlook.com>
2020-07-20 01:00:45 +0800

    [Zhu Mei][R1-1.5]add exception and tests

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -16,4 +16,19 @@ public class MenuTest {
     public void should_show_proper_output_when_listing_all_books() {
         menu.runOption("1");
     }
+
+    @Test(expected = InvalidOption.class)
+    public void should_throw_invalid_option_with_invalid_serial_2() throws InvalidOption {
+        menu.runOption("2");
+    }
+
+    @Test(expected = InvalidOption.class)
+    public void should_throw_invalid_option_with_invalid_serial_a() throws InvalidOption {
+        menu.runOption("a");
+    }
+
+    @Test(expected = InvalidOption.class)
+    public void should_throw_invalid_option_with_invalid_serial_01() throws InvalidOption {
+        menu.runOption("01");
+    }
 }
