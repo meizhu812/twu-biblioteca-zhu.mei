6bf2b8f47ee2c34d7e0429817e04dc61a5dcd2a2
Zhu Mei <meizhu812@outlook.com>
2020-07-20 01:38:17 +0800

    [Zhu Mei][R1-1.7]add more tests for checkout.

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -4,7 +4,8 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 public class LibraryTest {
     private Library library;
@@ -25,8 +26,26 @@ public class LibraryTest {
         assertSuccessCheckout("Foundation");
     }
 
+    @Test
+    public void should_fail_check_out_twice() {
+        assertSuccessCheckout("Foundation");
+        assertFailedCheckout("Foundation");
+    }
+
+    @Test
+    public void should_fail_check_out_non_existent_books() {
+        assertFailedCheckout("Harry Potter");
+        assertFailedCheckout("War and Peace");
+    }
+
     private void assertSuccessCheckout(String title) {
         assertEquals(library.checkOutBookByTitle(title)
+                .map(Book::getTitle)
+                .orElse(""), title);
+    }
+
+    private void assertFailedCheckout(String title) {
+        assertNotEquals(library.checkOutBookByTitle(title)
                 .map(Book::getTitle)
                 .orElse(""), title);
     }
