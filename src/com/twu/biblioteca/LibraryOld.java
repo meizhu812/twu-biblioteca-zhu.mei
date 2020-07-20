fdf9dd1f10ff2e34001c3cc0ec3c144a4d5fbc0e
Zhu Mei <meizhu812@outlook.com>
2020-07-21 00:14:18 +0800

    [Zhu Mei][R1-Refactor.I]Replace Library with LibraryNew. Cleaned up code.

---
+++ src/com/twu/biblioteca/LibraryOld.java
@@ -0,0 +1,50 @@
+package com.twu.biblioteca;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Deprecated
+public final class LibraryOld {
+    private final Map<String, Book> titleMap;
+    private Map<Book, Boolean> statusMap;
+
+    public LibraryOld(Collection<Book> initBooks) {
+        this.titleMap = initBooks.stream().collect(
+                Collectors.toMap(Book::getTitle, Function.identity(), (a, b) -> a));
+        init();
+    }
+
+    public void init() {
+        this.statusMap = this.titleMap.values().stream().collect(
+                Collectors.toMap(Function.identity(), book -> true, (a, b) -> a));
+    }
+
+    public List<Book> getAllBooks() {
+        return statusMap.entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Book> checkOutBookByTitle(String title) {
+        Optional<Book> bookOptional = Optional.ofNullable(titleMap.getOrDefault(title, null))
+                .filter(statusMap::get);
+        bookOptional.ifPresent(book -> statusMap.put(book, false));
+        return bookOptional;
+    }
+
+    public boolean returnBookByTitle(String title) {
+        Optional<Book> bookOptional = Optional.ofNullable(titleMap.getOrDefault(title, null))
+                .filter(book -> !statusMap.get(book));
+        if (bookOptional.isPresent()) {
+            statusMap.put(bookOptional.get(), true);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
