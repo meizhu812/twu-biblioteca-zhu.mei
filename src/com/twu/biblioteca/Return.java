a71adcfbadff85b78828cd7410ba1668197be4ea
Zhu Mei <meizhu812@outlook.com>
2020-07-20 22:59:28 +0800

    [Zhu Mei][R1-1.10,1.11,1.12]created return operation and info messages.

---
+++ src/com/twu/biblioteca/Return.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+public class Return<R> extends Operation<Boolean> {
+    public Return(Library library) {
+        super(library);
+    }
+
+    @Override
+    Boolean execute(String... input) {
+        return library.returnBookByTitle(input[0]);
+    }
+
+    @Override
+    void consoleExecute(String... input) {
+        if (execute(input)) {
+            System.out.println("Thank you for returning the book!");
+        } else {
+            System.out.println("That is not a valid book to return.");
+        }
+    }
+}
