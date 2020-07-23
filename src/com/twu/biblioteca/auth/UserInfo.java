eaa4b437acb2a2867c7630ed346587eb165c5d70
Zhu Mei <meizhu812@outlook.com>
2020-07-24 01:58:15 +0800

    [Zhu Mei][R2-2.4]add UserInfo class.

---
+++ src/com/twu/biblioteca/auth/UserInfo.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca.auth;
+
+public class UserInfo {
+    private String cardNo;
+    private String name;
+    private String email;
+    private String phoneNo;
+
+    public UserInfo(String cardNo, String name, String email, String phoneNo) {
+        this.cardNo = cardNo;
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getCardNo() {
+        return cardNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+}
